e81a3292f0e61c9003b0846dbd0dc141e53d719b
wizzard95 <damian110995a@gmail.com>
2026-03-17 18:18:48 -0300

    Primer commit

--- src/app/components/common/top-header/top-header.component.ts
+++ src/app/components/common/top-header/top-header.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-top-header',
+  standalone: true,
   imports: [],
   templateUrl: './top-header.component.html',
   styleUrl: './top-header.component.scss',
 })
 export class TopHeaderComponent {
-
+  
 }
