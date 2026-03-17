eae316b0cca4986c92f8fca187623e599c957f1d
wizzard95 <damian110995a@gmail.com>
2026-03-17 20:03:48 -0300

    config,ui(navbar): crear, configurar y estilizar el navbar para mostrar los enlaces determinados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopHeaderComponent } from "./components/common/top-header/top-header.component";
+import { NavbarComponent } from "./components/common/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    TopHeaderComponent],
+    TopHeaderComponent,
+    NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/common/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent {
+
+}
